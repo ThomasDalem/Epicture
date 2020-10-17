a8f223059f05d57301a0c44e364867e59d548da7
Marini Tony <Marini@desktop-o4f085k.home>
2020-10-17 12:27:49 +0200

    [ADD] get avatar of the user

--- epicture/lib/HomePage.dart
+++ epicture/lib/HomePage.dart
@@ -1,32 +1,19 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:provider/provider.dart';
+import 'dart:convert';
 
 import 'UserInfo.dart';
-
-//String getAccessToken(String url) {}
-
-String accessToken;
-String refreshToken;
 
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
 }
 
-class MyImage {
-  final String link;
-
-  MyImage({this.link});
-
-  factory MyImage.fromJson(Map<String, dynamic> json) {
-    return MyImage(
-      link: json['link'] as String,
-    );
-  }
-}
-
 class _HomePageState extends State<HomePage> {
+  String avatar;
   List<Image> listImage = [
     Image.network('https://picsum.photos/250?image=9'),
     Image.network('https://picsum.photos/250?image=1'),
@@ -37,31 +24,32 @@ class _HomePageState extends State<HomePage> {
   @override
   void initState() {
     final userInfos = Provider.of<UserInfo>(context, listen: false);
-    http.get("https://api.imgur.com/3/gallery/hot/viral/0.json", headers: {
-      "access_token": userInfos.accessToken,
-      "refresh_token": userInfos.refreshToken,
-      "token_type": "Bearer",
-      "account_username": userInfos.accountUsername,
+    avatar = "";
+    print(userInfos.accountUsername);
+    http.get("https://api.imgur.com/3/account/" + userInfos.accountUsername, headers: {
+      "Authorization": "Bearer " + userInfos.accessToken,
     }).then((response) {
-      //print('Response status: ${response.statusCode}');
-      print('Response body: ${response.body}');
       if (response.statusCode == 200) {
-        //var data = json.decode(response.body);
-        //var rest = data["link"] as String;
-        //var myImage = MyImage.fromJson(jsonDecode(response.body));
+        print(response.body);
+        var values = json.decode(response.body);
+        var data = values['data'];
+        setState(() {
+          avatar = data['avatar'];
+        });
+        Provider.of<UserInfo>(context, listen: false).getAvatar(data['avatar']);
       }
     });
   }
 
   Widget build(BuildContext context) {
+    final userInfos = Provider.of<UserInfo>(context, listen: false);
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Home Page'),
       ),
       backgroundColor: Color(0xFF3C3C3C),
-      body: ListView(
-        children: listImage,
-      ),
+      body: Image.network(userInfos.avatarUrl),
     );
   }
 }
