58ecdcae8cc11aa3f7d608fda7ea507d32089fbd
Thomas Dalem <thomas.dalem@epitech.eu>
2020-10-17 13:02:45 +0200

    [ADD]: list of top images in the home page

--- epicture/lib/HomePage.dart
+++ epicture/lib/HomePage.dart
@@ -1,10 +1,9 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:provider/provider.dart';
 
 import 'UserInfo.dart';
-
-//String getAccessToken(String url) {}
 
 String accessToken;
 String refreshToken;
@@ -14,43 +13,37 @@ class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
 }
 
-class MyImage {
-  final String link;
-
-  MyImage({this.link});
-
-  factory MyImage.fromJson(Map<String, dynamic> json) {
-    return MyImage(
-      link: json['link'] as String,
-    );
-  }
-}
-
 class _HomePageState extends State<HomePage> {
-  List<Image> listImage = [
-    Image.network('https://picsum.photos/250?image=9'),
-    Image.network('https://picsum.photos/250?image=1'),
-    Image.network('https://picsum.photos/250?image=2'),
-    Image.network('https://picsum.photos/250?image=8'),
-  ];
+  List<String> _imagesURL = List<String>();
 
   @override
   void initState() {
+    super.initState();
     final userInfos = Provider.of<UserInfo>(context, listen: false);
-    http.get("https://api.imgur.com/3/gallery/hot/viral/0.json", headers: {
-      "access_token": userInfos.accessToken,
-      "refresh_token": userInfos.refreshToken,
-      "token_type": "Bearer",
-      "account_username": userInfos.accountUsername,
+    print(userInfos.accessToken);
+    http.get("https://api.imgur.com/3/gallery/hot/viral/", headers: {
+      "Authorization": "Bearer ${userInfos.accessToken}"
     }).then((response) {
-      //print('Response status: ${response.statusCode}');
-      print('Response body: ${response.body}');
       if (response.statusCode == 200) {
-        //var data = json.decode(response.body);
-        //var rest = data["link"] as String;
-        //var myImage = MyImage.fromJson(jsonDecode(response.body));
+        final values = json.decode(response.body);
+        final data = values['data'];
+        setState(() {
+          for (var val in data) {
+            final images = val['images'];
+            if (images != null && images[0]['animated'] == false) {
+              print(images[0]);
+              _imagesURL.add(images[0]['link']);
+            }
+          }
+        });
       }
     });
+  }
+
+  ListTile imagesBuilder(BuildContext context, int index) {
+    return ListTile(
+      title: Image.network(_imagesURL[index]),
+    );
   }
 
   Widget build(BuildContext context) {
@@ -59,8 +52,9 @@ class _HomePageState extends State<HomePage> {
         title: Text('Home Page'),
       ),
       backgroundColor: Color(0xFF3C3C3C),
-      body: ListView(
-        children: listImage,
+      body: ListView.builder(
+        itemCount: _imagesURL.length,
+        itemBuilder: imagesBuilder,
       ),
     );
   }
